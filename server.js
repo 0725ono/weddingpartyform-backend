d0a6d4f4f0095278f1accff657a177e7b1a3c6f6
Masa Ono <ono.deutsch@gmail.com>
2023-09-11 21:07:18 +0900

    done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://weddingpartyform-frontend-egpja7iti-0725ono.vercel.app",
+    origin: "https://weddingpartyform-frontend-2cjr1crmv-0725ono.vercel.app",
   })
 );
 
